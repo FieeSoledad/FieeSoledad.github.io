18bd7aa18e2646cfaecdb30a77bdfa2785da9bac
Fieer <hsl1994364938@163.com>
2026-08-30 19:30:05 +0800

    Fix site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://fieesolelad.github.io',
+  site: 'https://fieesoledad.github.io',
   integrations: [mdx()],
 });
